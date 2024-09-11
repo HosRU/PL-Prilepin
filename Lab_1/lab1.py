5508b6243aeaa45e311b7cc3b48bd047967ee724
Artemounchik <Artem.fak@list.ru>
2024-09-11 10:32:19 +0300

    Выполнение программы

---
+++ Lab_1/lab1.py
@@ -0,0 +1,7 @@
+nameUser = input("Вводи своё имя и фамилию: ")
+ageUser = input("Напиши сюда свой возраст: ")
+cityUser = input("Ввиди сюда своё место жительство: ")
+
+print("Твоё имя: " + nameUser)
+print("Твой возраст: " + ageUser)
+print("Твой город проживания: " + cityUser)
