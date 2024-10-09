4dba3cd92ae1c2713e50ffb0d1dcdfde14223858
Artemounchik <Artem.fak@list.ru>
2024-10-09 10:51:09 +0300

    Задания+

---
+++ Laba |  AL/Задание 4/lab4_4.py
@@ -0,0 +1,6 @@
+array = [5,6,25,1,36,2]
+result = 0
+
+for i in array:
+    result = result + i
+    print(result)
---
+++ Laba |  AL/Задание 4/lab4_5.py
@@ -0,0 +1,6 @@
+number = 4
+result = 0
+
+for i in range(1, number+1):
+    result += i ** 3  
+    print(result)
