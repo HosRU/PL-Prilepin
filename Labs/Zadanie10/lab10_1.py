7ba0f3bca281e688c845623a527f9eed3fd97a3f
Artemounchik <Artem.fak@list.ru>
2024-11-20 11:08:57 +0300

    Обновочка2

--- Labs/Zadanie10/lab10_1.py
+++ Labs/Zadanie10/lab10_1.py
@@ -1,5 +1,13 @@
 import os
 # print(os.getcwd())
 
-with open("/home/artem/Тёмочка/PL-Prilepin/Labs/Zadanie9/lab9_15.py", 'r') as file:
-    text = file.readlines(5)
+text = []
+with open("/home/artem/Тёмочка/PL-Prilepin/Labs/Zadanie10/Prilepin_Y242_vivod.txt", 'r') as file:
+    for line in file:
+        lines = line.strip()
+        text.append(lines) 
+
+with open("/home/artem/Тёмочка/PL-Prilepin/Labs/Zadanie10/Prilepin_Y242_vvod.txt", 'w') as file:
+    file.write("Результат: \n")
+    for i in text:
+        file.write(f"{i} \n")
